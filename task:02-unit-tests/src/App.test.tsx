cb0cd1665a6bd86f4a7bbe3f1011e9559aa7c3df
Ebezer Igbinoba <igbinobaebenezer@gmail.com>
2023-01-24 18:14:56 +0100

    added new links

--- task:02-unit-tests/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Renders app with "Users text"', () => {
-  render(<App />);
-  const userHeader = screen.getByText(/Users/i);
-  expect(userHeader).toBeInTheDocument();
-});
--- task:02-unit-tests/src/config.ts
+++
@@ -1,4 +0,0 @@
-
-export const config = {
-    APP_BASE_URL: "https://jsonplaceholder.typicode.com"
-}
